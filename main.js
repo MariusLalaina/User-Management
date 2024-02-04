870478dacc92bc5a9b19480aae3553929c7d66ab
marius <mariuslalaina.fanomezantsoa@gmail.com>
2024-02-04 16:31:16 +0300

    adding modal by js

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const profil = document.querySelector('.profil');
 const action = document.querySelector('.action');
+const avatar = document.querySelector('.avatar');
+const form = document.querySelector('.form');
+const add = document.querySelector('.add');
+const done = document.querySelector('.done');
+const close = document.querySelector('.close');
 
 profil.addEventListener("mouseenter", function(){
-    action.style.display ="flex"
+    action.style.display ="flex";
 })
 profil.addEventListener("mouseleave", function(){
-  action.style.display ="none"
+  action.style.display ="none";
+})
+add.addEventListener('click',function(){
+  form.showModal();
+  form.style.display = "flex";
+  form.style.flexDirection = "column";
+  form.style.gap = "10px";
+
+
+})
+close.addEventListener('click', function(){
+  form.close();
+  form.style.display = "none";
 })
