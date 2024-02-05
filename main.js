9dda5a5da37feeaee05eb470b2ea7db6295c748b
marius <mariuslalaina.fanomezantsoa@gmail.com>
2024-02-05 13:38:45 +0300

    creating action into profil

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// const profil = document.querySelector('.profil');
+const profil = document.querySelector('.profil');
 // const action = document.querySelector('.action');
 // const avatar = document.querySelector('.avatar');
 const form = document.querySelector('.form');
@@ -8,16 +8,9 @@ const close = document.querySelector('.close');
 const profilAvatar = document.querySelector('.profil_avatar');
 const po = document.querySelector('.user');
 const mail = document.querySelector('.mail');
-const trash = document.querySelector('.trash');
-const edit = document.querySelector('.edit');
+
 const cards = document.querySelector('.cards');
 
-/*profil.addEventListener("mouseenter", function(){
-    action.style.display ="flex";
-})
-profil.addEventListener("mouseleave", function(){
-  action.style.display ="none";
-})*/
 add.addEventListener('click', function () {
   form.showModal();
   form.style.display = "flex";
@@ -28,7 +21,10 @@ done.addEventListener('click', function () {
   let profil = document.createElement('div');
   let info = document.createElement('div');
   let action = document.createElement('div');
-  
+  let trash = document.createElement('p');
+  let edit = document.createElement('p');
+  trash.textContent = "Suppr";
+  edit.textContent = "Edit";
   profil.classList.add("profil");
   info.classList.add("info");
   action.classList.add("action");
@@ -36,7 +32,11 @@ done.addEventListener('click', function () {
   let p1 = document.createElement('p');
   let p2 = document.createElement('p');
   img.classList.add("avatar");
-  img.src = profilAvatar.value;
+  var reader = new FileReader();
+    reader.onload = function(e) {
+        img.src = e.target.result;
+    };
+    reader.readAsDataURL(profilAvatar.files[0]); 
   img.alt = "Image";
   p1.textContent = po.value;
   p2.textContent = mail.value;
@@ -51,9 +51,17 @@ done.addEventListener('click', function () {
   profil.appendChild(info);
   profil.appendChild(action);
   cards.appendChild(profil);
+  profil.addEventListener("mouseenter", function(){
+    action.style.display ="flex";
+})
+profil.addEventListener("mouseleave", function(){
+  action.style.display ="none";
+})
+form.style.display = "none";
 })
 close.addEventListener('click', function () {
   form.close();
   form.style.display = "none";
 })
+
 console.log(cards);
